02cb83a1045e361a020317cb18086c06eebc6017
Egor Boiko <boyko.egor18@gmail.com>
2020-11-03 14:37:00 +0200

    решил 2ю задачку

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,10 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
-  // ваш код...
+function factorial(n){
+  let calc = 1;
+for(let i = 2; i<=n; i++){
+  calc *= i;
+} 
+  return calc;
 }
