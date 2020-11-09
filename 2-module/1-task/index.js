6012481020a65a0545ab413f216a6972b1df19bd
Egor Boiko <boyko.egor18@gmail.com>
2020-11-09 14:54:28 +0200

    решил превую задачу 2го модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,15 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
-  // ваш код...
+
+ function sumSalary(Object) {
+  let sum = 0;
+  for(let key in Object){
+      if(typeof( Object[key] ) === 'number'){
+      sum += Object[key] } 
+      else if( typeof(Object[key] ) === 'any'){
+      return} 
+      
+  }
+  return sum;
 }
